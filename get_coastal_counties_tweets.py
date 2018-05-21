f64d5c01d76312689682e8cbc0d4fec6b3046043
Rudy Huezo <rahuezo@ucdavis.edu>
2018-05-21 08:45:26 -0700

    Fixed results path

--- get_coastal_counties_tweets.py
+++ get_coastal_counties_tweets.py
@@ -23,23 +23,25 @@ if __name__== "__main__":
     for i, tweet_db_file in enumerate(tweet_db_files): 
         print '{} out of {} databases'.format(i + 1, len(tweet_db_files))
 
-        current_coastal_counties_db = Database(os.path.join(RESULTS_PATH, tweet_db_file))        
-        coastal_counties_tweets_table = current_coastal_counties_db.create_table(*COASTAL_COUNTIES_TWEETS_TABLE)
-
-        joined_rows = coastal_counties_db.ijoin((tweet_db_file, 'other_db', 'tweets'), FIELDS_TO_SELECT_FOR_JOIN + ',counties.fips', MATCH_CRITERIA_FOR_JOIN)
-
-        print "Got rows"
-
-        current_coastal_counties_db.cursor.execute('BEGIN')
+        print "DB Outpath", os.path.join(RESULTS_PATH, tweet_db_file)
         
-        print "Inserting rows"
-        current_coastal_counties_db.insert("""INSERT INTO {} 
-            VALUES(?,?,?,?,?)""".format(coastal_counties_tweets_table), joined_rows, many=True)
+        # current_coastal_counties_db = Database(os.path.join(RESULTS_PATH, tweet_db_file))        
+        # coastal_counties_tweets_table = current_coastal_counties_db.create_table(*COASTAL_COUNTIES_TWEETS_TABLE)
 
-        print "Commiting rows"
-                                
-        current_coastal_counties_db.connection.commit()
-        current_coastal_counties_db.connection.close()
+        # joined_rows = coastal_counties_db.ijoin((tweet_db_file, 'other_db', 'tweets'), FIELDS_TO_SELECT_FOR_JOIN + ',counties.fips', MATCH_CRITERIA_FOR_JOIN)
+
+        # print "Got rows"
+
+        # current_coastal_counties_db.cursor.execute('BEGIN')
+        
+        # print "Inserting rows"
+        # current_coastal_counties_db.insert("""INSERT INTO {} 
+        #     VALUES(?,?,?,?,?)""".format(coastal_counties_tweets_table), joined_rows, many=True)
+
+        # print "Commiting rows"
+
+        # current_coastal_counties_db.connection.commit()
+        # current_coastal_counties_db.connection.close()
 
     coastal_counties_db.connection.close()
 
--- utils/configuration.py
+++ utils/configuration.py
@@ -1,12 +1,14 @@
 import os
 
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).replace('utils', '')
+
 TWEETS_TABLENAME = 'tweets'
 FIELDS_TO_SELECT_FOR_JOIN = 'user_id, tweet_text, tweet_location, tweet_date'
 MATCH_CRITERIA_FOR_JOIN = 'fips'
 COASTAL_COUNTIES_TWEETS_TABLE = ('tweets', 'user_id INT, tweet_text TEXT, tweet_location TEXT, tweet_date TEXT, fips TEXT')
 
-RESULTS_PATH = 'results'
+RESULTS_PATH = os.path.join(ROOT_DIR, 'results')
 
 
 
