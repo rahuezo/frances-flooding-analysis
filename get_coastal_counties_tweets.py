8697d8caaf0c3255f677104309b8fea8b1705715
Rudy Huezo <rahuezo@ucdavis.edu>
2018-05-23 08:14:11 -0700

    Fixed results path discrepancy

--- get_coastal_counties_tweets.py
+++ get_coastal_counties_tweets.py
@@ -1,4 +1,4 @@
-from utils.configuration import (FIELDS_TO_SELECT_FOR_JOIN, MATCH_CRITERIA_FOR_JOIN, RESULTS_PATH, COASTAL_COUNTIES_TWEETS_TABLE)
+from utils.configuration import (FIELDS_TO_SELECT_FOR_JOIN, MATCH_CRITERIA_FOR_JOIN, COASTAL_COUNTIES_TWEETS_TABLE)
 from utils.database import Database
 
 
@@ -6,7 +6,10 @@ import tkFileDialog as fd
 import time, sys, os
 
 
-if __name__== "__main__": 
+RESULTS_PATH = os.path.join(os.getcwd(), 'results')
+
+
+if __name__== "__main__":
     try: 
         coastal_counties_db_file = fd.askopenfilename(title="Choose coastal counties database")
         tweet_db_files = fd.askopenfilenames(title="Choose ALL databases with tweets")
@@ -23,27 +26,26 @@ if __name__== "__main__":
     for i, tweet_db_file in enumerate(tweet_db_files): 
         print '{} out of {} databases'.format(i + 1, len(tweet_db_files))
 
-        print "DB Outpath", os.path.join(RESULTS_PATH, tweet_db_file)
+        current_coastal_counties_db = Database(os.path.join(RESULTS_PATH, tweet_db_file))        
+        coastal_counties_tweets_table = current_coastal_counties_db.create_table(*COASTAL_COUNTIES_TWEETS_TABLE)
+
+        joined_rows = coastal_counties_db.ijoin((tweet_db_file, 'other_db', 'tweets'), FIELDS_TO_SELECT_FOR_JOIN + ',counties.fips', MATCH_CRITERIA_FOR_JOIN)
+
+        print "Got rows"
+
+        current_coastal_counties_db.cursor.execute('BEGIN')
         
-        # current_coastal_counties_db = Database(os.path.join(RESULTS_PATH, tweet_db_file))        
-        # coastal_counties_tweets_table = current_coastal_counties_db.create_table(*COASTAL_COUNTIES_TWEETS_TABLE)
+        print "Inserting rows"
+        current_coastal_counties_db.insert("""INSERT INTO {} 
+            VALUES(?,?,?,?,?)""".format(coastal_counties_tweets_table), joined_rows, many=True)
 
-        # joined_rows = coastal_counties_db.ijoin((tweet_db_file, 'other_db', 'tweets'), FIELDS_TO_SELECT_FOR_JOIN + ',counties.fips', MATCH_CRITERIA_FOR_JOIN)
+        print "Commiting rows"
 
-        # print "Got rows"
-
-        # current_coastal_counties_db.cursor.execute('BEGIN')
-        
-        # print "Inserting rows"
-        # current_coastal_counties_db.insert("""INSERT INTO {} 
-        #     VALUES(?,?,?,?,?)""".format(coastal_counties_tweets_table), joined_rows, many=True)
-
-        # print "Commiting rows"
-
-        # current_coastal_counties_db.connection.commit()
-        # current_coastal_counties_db.connection.close()
+        current_coastal_counties_db.connection.commit()
+        current_coastal_counties_db.connection.close()
 
     coastal_counties_db.connection.close()
+    
 
 
 
