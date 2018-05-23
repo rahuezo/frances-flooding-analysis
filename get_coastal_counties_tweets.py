b35da003a1a74766f6e017d418c3e5aec500f592
Rudy Huezo <rahuezo@ucdavis.edu>
2018-05-23 08:31:12 -0700

    Testing current coastal county db path

--- get_coastal_counties_tweets.py
+++ get_coastal_counties_tweets.py
@@ -34,7 +34,7 @@ if __name__== "__main__":
         joined_rows = coastal_counties_db.ijoin((tweet_db_file, 'other_db', 'tweets'), FIELDS_TO_SELECT_FOR_JOIN + ',counties.fips', MATCH_CRITERIA_FOR_JOIN)
 
         print "Current Results Path: ", RESULTS_PATH
-
+        print "Current coastal county db path: ", os.path.join(RESULTS_PATH, tweet_db_file)
         # print "Got rows"        
 
         current_coastal_counties_db.cursor.execute('BEGIN')
