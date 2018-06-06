957192aac926fbb0b7d466bbafdd73c076f176de
Rudy Huezo <rahuezo@ucdavis.edu>
2018-06-06 06:53:14 -0700

    Added date splitting

--- split_tweet_date_column.py
+++ split_tweet_date_column.py
@@ -7,9 +7,18 @@ import multiprocessing as mp
 
 
 RESULTS_PATH = os.path.join(os.getcwd(), 'results')
-KEYWORDS = [sanitize_string(kw[0].lower()) for kw in csv.reader(open(fd.askopenfilename(title="Choose keywords file"), 'rb'), delimiter=',')]
 
-UPDATE_COLUMN_COMMAND = """UPDATE tweets SET about_flood=1 WHERE {}""".format(" OR ".join(["tweet_text LIKE '%{}%'".format(kw) for kw in KEYWORDS]))
+UPDATE_COLUMN_COMMAND = """
+    ALTER TABLE tweets ADD COLUMN year INT; 
+    ALTER TABLE tweets ADD COLUMN month INT; 
+    ALTER TABLE tweets ADD COLUMN day INT; 
+    ALTER TABLE tweets ADD COLUMN hour INT; 
+
+    UPDATE tweets SET year = SUBSTR(tweet_date, 0, 5); 
+    UPDATE tweets SET month = SUBSTR(tweet_date, 6, 2);
+    UPDATE tweets SET day = SUBSTR(tweet_date, 9, 2);
+    UPDATE tweets SET hour = SUBSTR(tweet_date, 12, 2);
+"""
 
 
 if not os.path.exists(RESULTS_PATH): 
@@ -27,7 +36,6 @@ if __name__== "__main__":
         sys.exit()
 
     total_time = 0 
-    total_time_loop = 0
 
     for i, tweet_db_file in enumerate(coastal_counties_tweets_db_files): 
         print '{} out of {} databases'.format(i + 1, len(coastal_counties_tweets_db_files))
@@ -37,7 +45,7 @@ if __name__== "__main__":
         current_tweet_db = Database(tweet_db_file)
 
         current_tweet_db.cursor.execute('BEGIN')
-        current_tweet_db.cursor.execute(UPDATE_COLUMN_COMMAND) # add new about_flood column default 0
+        current_tweet_db.cursor.executescript(UPDATE_COLUMN_COMMAND) # split date into year, month, day, hour
         current_tweet_db.connection.commit()
     
 
@@ -50,6 +58,20 @@ if __name__== "__main__":
 
     print "\nTotal time: {}s".format(round(total_time, 2))
 
+
 # UPDATE 
 # tweets SET about_flood = CASE WHEN tweet_text like "%been%" THEN 1 ELSE 0
 # END
+
+
+
+# ALTER TABLE tweets ADD COLUMN year INT; 
+# ALTER TABLE tweets ADD COLUMN month INT; 
+# ALTER TABLE tweets ADD COLUMN day INT; 
+# ALTER TABLE tweets ADD COLUMN hour INT; 
+
+
+# UPDATE tweets SET year = SUBSTR(tweet_date, 0, 5); 
+# UPDATE tweets SET month = SUBSTR(tweet_date, 6, 2);
+# UPDATE tweets SET day = SUBSTR(tweet_date, 9, 2);
+# UPDATE tweets SET hour = SUBSTR(tweet_date, 12, 2);
