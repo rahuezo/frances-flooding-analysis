0a644cb2d2a05f2c474c90a9df3c0fcf2be65b4e
rahuezo <rahuezo@ucdavis.edu>
2019-05-02 14:58:46 -0700

    Added get_tweets_by_county

---
+++ get_tweets_by_county.py
@@ -0,0 +1,39 @@
+from utils.database import Database
+
+import tkFileDialog as fd
+import time, sys, os
+import multiprocessing as mp
+
+relevant_counties = ["48245", "48361"]
+
+
+if __name__== "__main__":
+    try: 
+        tweet_db_files = fd.askopenfilenames(title="Choose ALL databases with tweets")
+
+        if not tweet_db_files: 
+            raise Exception('\nNo databases selected! Goodbye.\n')
+    except Exception as e: 
+        print e
+        sys.exit()
+
+
+    for i, tweet_db_file in enumerate(tweet_db_files): 
+        print '{} out of {} databases'.format(i + 1, len(tweet_db_files))
+
+        current_db = Database(tweet_db_file)
+
+        for row in current_db.select("""SELECT * FROM tweets WHERE fips IN ("{}", "{}") AND about_flood=1"""): 
+            print row
+
+        
+
+        
+    
+
+
+
+
+
+
+
